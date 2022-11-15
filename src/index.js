94a6be9f94d1fdc31d2b6527d8485de06142d9f3
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-11-15 17:18:49 -0300

    O que é Middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const router = require('./routes');
-const { use } = require('./routes');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.use(router);
+app.use(routes);
 
 app.listen(3000, () =>
   console.log('🔥 Server started at http://localhost:3000'),
