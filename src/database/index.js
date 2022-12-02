4b093745c63788323f38068cdff9433ff44eebab
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-02 15:53:29 -0300

    Criando o middleware do CORS na API

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 const client = new Client({
   host: 'localhost',
   port: 5432,
-  user: 'jcatulio',
+  user: 'root',
   password: 'root',
   database: 'mycontacts',
 });
