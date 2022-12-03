ae9bc64a28d6ef59b7ac16b3e8fe0f9b85204570
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-03 18:00:16 -0300

    Códigos de status HTTP e ajustes no Back-end

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -14,10 +14,10 @@ class ContactController {
     const contact = await ContactsRepository.findById(id);
 
     if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
+      return response.status(404).json({ error: 'Contact not found' });
     }
 
-    response.json(contact);
+    return response.json(contact);
   }
 
   async store(request, response) {
@@ -42,7 +42,7 @@ class ContactController {
       category_id,
     });
 
-    response.json(contact);
+    response.status(201).json(contact);
   }
 
   async update(request, response) {
@@ -51,7 +51,7 @@ class ContactController {
 
     const contactExists = await ContactsRepository.findById(id);
     if (!contactExists) {
-      return response.status(404).json({ error: 'User not found' });
+      return response.status(404).json({ error: 'Contact not found' });
     }
 
     if (!name) {
